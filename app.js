3d06cea1f2cee4f6fdf7fd5a8e461a22643d38e8
Juanma <juanmasanchez1905@gmail.com>
2024-04-29 13:12:12 -0500

    commit 5

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
-app.use(express.json);
+app.use(express.json());
 
 const dotenv = require("dotenv");
 dotenv.config({ path: "./env/.env" });
@@ -24,10 +24,87 @@ app.use(
   })
 );
 
-const connection = require("./database/db"); 
+const connection = require("./database/db");
 
 app.get("/", (req, res) => {
-  res.send("HOla mundo");
+  res.render("index");
+});
+app.get("/index", (req, res) => {
+  res.render("index");
+});
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+app.get("/register", (req, res) => {
+  res.render("register");
+});
+
+app.post("/register", async (req, res) => {
+  const username = req.body.username;
+  const email = req.body.email;
+  const password = req.body.password;
+  let passwordHash = await bcryptjs.hash(password, 8);
+  connection.query(
+    "INSERT INTO usuario SET ?",
+    {
+      username: username,
+      email: email,
+      password: passwordHash,
+    },
+    async (error, results) => {
+      if (error) {
+        console.log(error);
+      } else {
+        res.render("register", {
+          alert: true,
+          alertTitle: "Registro",
+          alertMessage: "¡Registro exitoso!",
+          alertIcon: "success",
+          showConfirmButton: true,
+          timer: false,
+          ruta: "",
+        });
+      }
+    }
+  );
+});
+
+app.post("/auth", async (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  let passwordHash = await bcryptjs.hash(password, 8);
+  if (username && password) {
+    connection.query(
+      "SELECT * FROM usuario WHERE username = ?",
+      [username],
+      async (error, results) => {
+        if (
+          results.length == 0 ||
+          !(await bcryptjs.compare(password, results[0].password))
+        ) {
+          res.render("login", {
+            alert: true,
+            alertTitle: "Error",
+            alertMessage: "Usuario y/o contraseña incorrectos",
+            alertIcon: "error",
+            showConfirmButton: true,
+            timer: false,
+            ruta: "login",
+          });
+        } else {
+          res.render("login", {
+            alert: true,
+            alertTitle: "Ingreso",
+            alertMessage: "¡Ingreso Exitoso!",
+            alertIcon: "success",
+            showConfirmButton: true,
+            timer: false,
+            ruta: "",
+          });
+        }
+      }
+    );
+  }
 });
 
 app.listen(3000, (req, res) => {
